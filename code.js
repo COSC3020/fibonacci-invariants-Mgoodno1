e16a3423b3902d1b116365261bb49882c064cb15
Mgoodno1 <90008902+Mgoodno1@users.noreply.github.com>
2024-12-02 15:38:05 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function fib(n) {
-    return n;
+    if (n < 0) {
+        return "Invalid Input";
+    }
+    if (n === 0) {
+        return [0];
+    }
+    if (n === 1) {
+        return [0, 1];
+    }
+
+    const answer = fib(n - 1);
+    const nextFib = answer[answer.length - 1] + answer[answer.length - 2];
+    answer.push(nextFib);
+    return answer;
 }
